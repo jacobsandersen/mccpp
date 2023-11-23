d84104862a74b635a3a9ace521294c94622eaa92
Jacob Andersen <19645494+simpleauthority@users.noreply.github.com>
2023-11-22 16:21:22 -0800

    generify NBTBuilder

--- src/nbt/NBTBuilder.h
+++ src/nbt/NBTBuilder.h
@@ -9,6 +9,10 @@
 using std::shared_ptr, std::make_shared;
 
 namespace NBT {
+    template <typename T> using is_tag = std::is_base_of<Tag<typename std::decay_t<T>::type>, typename std::decay_t<T>::type>;
+    template <typename T> using is_no_param_tag = std::disjunction<std::is_same<TagCompound, typename std::decay_t<T>::type>, std::is_same<TagEnd, typename std::decay_t<T>::type>>;
+    template <typename T> using is_param_tag = std::conjunction<is_tag<std::decay_t<T>>, std::negation<is_no_param_tag<std::decay_t<T>>>>;
+
     class NBTBuilder : public std::enable_shared_from_this<NBTBuilder> {
     public:
         static shared_ptr<NBTBuilder> create() {
@@ -16,85 +20,33 @@ namespace NBT {
         }
 
         shared_ptr<NBTBuilder> add_tag_end() {
-            if (open_tags == 0) {
+            if (m_open_tags == 0) {
                 throw std::runtime_error("Tried to add TAG_End, but no TAG_Compounds are open.");
             }
 
-            open_tags--;
+            m_open_tags--;
 
             TagEnd end;
             end.write(m_buffer);
         }
 
-        shared_ptr<NBTBuilder> add_tag_byte(std::string name, int8_t value) {
-            TagByte t(std::move(name), value);
-            t.write(m_buffer);
-            return shared_from_this();
-        }
-
-        shared_ptr<NBTBuilder> add_tag_short(std::string name, int16_t value) {
-            TagShort t(std::move(name), value);
-            t.write(m_buffer);
-            return shared_from_this();
-        }
-
-        shared_ptr<NBTBuilder> add_tag_int(std::string name, int32_t value) {
-            TagInt t(std::move(name), value);
-            t.write(m_buffer);
-            return shared_from_this();
-        }
-
-        shared_ptr<NBTBuilder> add_tag_long(std::string name, int64_t value) {
-            TagLong t(std::move(name), value);
-            t.write(m_buffer);
-            return shared_from_this();
-        }
-
-        shared_ptr<NBTBuilder> add_tag_float(std::string name, float value) {
-            TagFloat t(std::move(name), value);
-            t.write(m_buffer);
-            return shared_from_this();
-        }
-
-        shared_ptr<NBTBuilder> add_tag_double(std::string name, double value) {
-            TagDouble t(std::move(name), value);
-            t.write(m_buffer);
-            return shared_from_this();
-        }
-
-        shared_ptr<NBTBuilder> add_tag_byte_array(std::string name, std::vector<int8_t> value) {
-            TagByteArray t(std::move(name), std::move(value));
-            t.write(m_buffer);
-            return shared_from_this();
-        }
-
-        shared_ptr<NBTBuilder> add_tag_string(std::string name, std::string value) {
-            TagString t(std::move(name), std::move(value));
-            t.write(m_buffer);
-            return shared_from_this();
-        }
-
-        template <typename T, typename = std::enable_if<std::is_base_of_v<Tag<T>, T>>> shared_ptr<NBTBuilder> add_tag_list(std::string name, std::vector<T> value) {
-            TagList<T> t(std::move(name), std::move(value));
-            t.write(m_buffer);
-            return shared_from_this();
-        }
-
         shared_ptr<NBTBuilder> add_tag_compound(std::string name) {
             TagCompound t(std::move(name));
             t.write(m_buffer);
-            open_tags++;
+            m_open_tags++;
             return shared_from_this();
         }
 
-        shared_ptr<NBTBuilder> add_tag_int_array(std::string name, std::vector<int32_t> value) {
-            TagIntArray t(std::move(name), std::move(value));
+        // only defined if T is a Tag that takes parameters (i.e., not TagCompound, not TagEnd)
+        template <typename T, typename ValueType = std::enable_if_t<is_param_tag<T>::value>> std::shared_ptr<NBTBuilder> add(std::string name, ValueType value) {
+            T t(std::move(name), value);
             t.write(m_buffer);
             return shared_from_this();
         }
 
-        shared_ptr<NBTBuilder> add_tag_long_array(std::string name, std::vector<int64_t> value) {
-            TagLongArray t(std::move(name), std::move(value));
+        // only defined if T is a TagList holding ValueType, and ValueType is a Tag.
+        template <typename T, typename ValueType, typename = std::enable_if_t<std::conjunction_v<std::is_same<TagList<ValueType>, T>, is_tag<ValueType>>, T>> std::shared_ptr<NBTBuilder> add(std::string name, std::vector<ValueType> value) {
+            TagList<ValueType> t(std::move(name), std::move(value));
             t.write(m_buffer);
             return shared_from_this();
         }
@@ -103,10 +55,10 @@ namespace NBT {
             return m_buffer;
         }
     private:
-        NBTBuilder() : m_buffer(), open_tags(0) {}
+        NBTBuilder() : m_buffer(), m_open_tags(0) {}
 
         ByteBuffer m_buffer;
-        uint32_t open_tags;
+        uint32_t m_open_tags;
     };
 }
 
